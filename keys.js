39d8764fdc786d41d7236c03e3f852a1b37b32ed
unknown <dipisha03@gmail.com>
2018-01-20 19:14:09 -0500

    last-commit

---
+++ keys.js
@@ -0,0 +1,18 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'QHQ9Daz8f5ysCa0cMwAbkj13y',
+  consumer_secret: 'x9eXmt0QVjRc1gb6jbsWvMmvQh3bgtu135ISMozrUvFRipu32b',
+  access_token_key: '953489740619239424-00YYiP67TaCaN0uNAX6We4Yv3kCZxoo',
+  access_token_secret: 'sRbGkkKJqRBY1NtwJD02K9xeF5MKXC2OB7FoNAAL5rAJx',
+}
+
+exports.spotifyKeys = {
+	id: 'aa6388f3a35d4448aeb3bea124d8f26b',
+	secret: 'e05176c032d64722a929012039b66e02'
+}
+
+
+
+
